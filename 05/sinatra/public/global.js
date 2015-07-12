753a39ff02963f77fab91fbd9bb10061e835e71e
Andrew Lodes <alodes999@gmail.com>
2015-07-11 23:12:35 -0500

    Finish Atomic 5 (no stretches yet)

--- 05/sinatra/public/global.js
+++ 05/sinatra/public/global.js
@@ -1,5 +1,25 @@
 var finishLinks = document.getElementsByClassName("done_link");
 
+var mark_task_done = function(event) {
+  var link_to_use = this;
+  
+  var req = new XMLHttpRequest();
+  
+  req.open("get", link_to_use.getAttribute("href"));
+  
+  req.addEventListener("load", function(){    
+    link_to_use.parentElement.classList.add("finished");
+    var child_to_remove = link_to_use.parentNode.childNodes[1];
+    link_to_use.parentNode.removeChild(child_to_remove);
+  });
+
+  req.send();
+  
+  // Prevent the link from refreshing the DOM.
+  event.preventDefault();
+}
+
 for (var i=0; i < finishLinks.length; i++) {
   // finishLinks[i] represents each of the items in `finishLinks`.
+  finishLinks[i].addEventListener("click", mark_task_done)
 }
